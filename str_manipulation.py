535a57c2a8eb5509c481e7f8872ca862d88543a3
unknown <karkisugam34@gmail.com>
2024-12-26 18:22:51 +0545

    first commit

---
+++ str_manipulation.py
@@ -0,0 +1,18 @@
+# each char in string moved 2 step forward. eg.: hacker -> erhack
+
+inp = input("Enter a string: ")
+
+# other necessities
+steps = 2
+str_len = len(inp)
+
+# Processing
+temp = [None] * str_len
+for i in range(str_len):
+    temp[(i + steps) % str_len] = inp[i]
+
+# Convert list back to a string
+result = ''.join(temp)
+
+# Output
+print("Output:", result)
